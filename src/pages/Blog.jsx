89c19fbc19bc50e404d735567bef0586c71a6ece
BethTawiah <elizabeth.tawiah@amalitech.org>
2025-06-04 22:19:22 +0000

    added like useState

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Delete, Heart, Trash } from "lucide-react";
 
 const API_URL = "http://localhost:3000/blogs";
 
@@ -7,6 +8,8 @@ function Blog() {
   const [blogs, setBlogs] = useState([]);
   const [isExpandText, setIsExpandText] = useState(false);
   const [blogClicked, setBlogClicked] = useState(null);
+  const [isDelete, setIsDelete] = useState(false);
+  const [like, setLike] = useState(false);
 
   // fetch data from api store in BLOGS
   useEffect(() => {
@@ -22,18 +25,47 @@ function Blog() {
     fetchBlogs();
   }, []);
 
+  //Handle read more text
   function handleExpandText(blog) {
     setIsExpandText(!isExpandText); //toddle isExapandText State
     setBlogClicked(blog); //set the blog that is clicked and shows the full paragraph at right
   }
+
+  //Handle delete blog
+  const handleDeleteBlog = async(blogId)=>{
+    try {
+       const confirmDelete = window.confirm("Are you sure you want to delete this blog?");
+    if(confirmDelete){
+          await axios.delete(API_URL + `/${blogId}`) //delete each blog(represents with an id) from the api
+          setBlogs(blogs.filter(blog => blog.id !== blogId)); //Removes  the deleted blog from the  blog list
+          setIsDelete(true); 
+    }
+    } catch (err) {
+      console.log(err)
+      
+    }
+
+  }
+
+
+  //Handle Like a Blog
+  const handleLikeBlog =  (blogId ) =>{
+    setLike(!like);
+    
+  }
   return (
     <div className="flex  flex-row  ">
-      <div className="flex  flex-wrap justify-center items-center w-1/2 h-full">
+      <div className="flex  flex-wrap justify-center items-center w-1/2  bg-blue-200 h-full">
         {blogs.map((blog, index) => (
           <div
-            className="flex flex-col bg-blue-800 w-[20vw] text-white h-[20vh] shadow-lg shadow-blue-500/50  border-amber-500 rounded-xl my-20 mx-40 justify-center p-10"
+            className="flex flex-col bg-blue-800 w-[40vw] text-white h-[20vh] shadow-lg shadow-blue-500/50  border-amber-500 rounded-xl  mx-40  my-10 justify-center p-10"
             key={index}
           >
+            <div className="flex justify-end items-center hover:  mb-2 ">
+              <Heart className="cursor-pointer gap-2" onClick={()=>handleLikeBlog(blog.id)}  color={like ? "red" : "white"} fill={like ? "red" : "none"}/>
+                 {/* colour === red if like is true else white same as fill */}
+              <Trash className="cursor-pointer" onClick={() =>handleDeleteBlog(blog.id)}/>
+            </div>
             {/* <img
               className="rounded-t-lg"
               src={blog.imgAttach}
@@ -44,9 +76,9 @@ function Blog() {
               {blog.title}
             </div>
             <div className="text-left text-sm font-normal tracking-tight cursor-default">
-              
 
-              {blog.description.length > 10 && (
+              {/* show read more or read less depending on the text length */}
+              {blog.description.length > 30 && (
                 <span
                   className="text-yellow-500 ml-1 cursor-pointer inline-block"
                   onClick={() => handleExpandText(blog)}
@@ -58,17 +90,21 @@ function Blog() {
           </div>
         ))}
       </div>
-      <div className="flex  flex-wrap justify-center items-center w-1/2 h-full">
-        <div className="flex flex-col bg-blue-800 text-white h-[50vh] shadow-lg shadow-blue-500/50  border-amber-500 rounded-xl my-20 mx-40 justify-center p-10">
+
+      {/* <h1 className="text-4xl justify-center text-blue-800 font-bold"> Read Full Story</h1> */}
+
+      <div className="flex   justify-center items-center w-1/2 h-[100vh]">
+        <div className="flex flex-col  text-blue-800 h-[60vh] w-[70vw] shadow-lg shadow-blue-500/50  border-amber-500 rounded-xl my-20 mx-10 justify-center p-10">
           {blogClicked && (
-            <>
-              <div className="text-left text-2xl  mb-2 font-bold tracking-tight">
+            //Show this when a blog is clicked
+            <div>
+              <div className="text-left text-2xl  mb-4 font-bold tracking-tight">
                 {blogClicked.title}
               </div>
-              <div className="text-left text-sm font-normal tracking-tight cursor-default">
+              <div className="text-left text-lg font-medium tracking-tight cursor-default">
                 {blogClicked.description}
               </div>
-            </>
+            </div>
           )}
         </div>
       </div>
